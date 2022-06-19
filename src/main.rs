c2ca8b4eef2628e93d2f0ce19b00e420dc9d9c3f
Ben Smith <23388317+Bensmith9722@users.noreply.github.com>
2022-06-19 04:40:55 -0500

    Adds comparing RNG number and guessed number.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
@@ -13,5 +15,16 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
     
+    let guess: u32 = guess
+                     .trim()
+                     .parse()
+                     .expect("Please type a number!");
+                     
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => print!("Too small!"),
+        Ordering::Greater => print!("Too big!"),
+        Ordering::Equal => print!("You win!"),
+    }
 }
